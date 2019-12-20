19bf558e1c68e4a25ba5bf7a1bd4e46cf2b7d208
ukhod <45069228+ukhod@users.noreply.github.com>
2019-12-21 01:45:18 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Scrabble_Disctionary
+{
+    partial class frmScrabble_dictionary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_word = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Find Word:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_word
+            // 
+            this.txt_word.Location = new System.Drawing.Point(152, 53);
+            this.txt_word.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_word.Name = "txt_word";
+            this.txt_word.Size = new System.Drawing.Size(178, 20);
+            this.txt_word.TabIndex = 2;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_search.Location = new System.Drawing.Point(229, 98);
+            this.btn_search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(100, 24);
+            this.btn_search.TabIndex = 3;
+            this.btn_search.Text = "Search Word";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            this.btn_search.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmScrabble_dictionary_KeyDown);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(117, 98);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(100, 24);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmScrabble_dictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 156);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_word);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "frmScrabble_dictionary";
+            this.Text = "Scrabble Dictionary";
+            this.Load += new System.EventHandler(this.frmScrabble_dictionary_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmScrabble_dictionary_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_word;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Scrabble_Disctionary
+{
+    public partial class frmScrabble_dictionary : Form
+    {
+        
+        public frmScrabble_dictionary()
+        {
+            InitializeComponent();
+          
+        }
+        private void label1_Click(object sender, EventArgs e)
+        { }
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+
+            txt_word.Text = txt_word.Text.ToUpper();
+            if (string.Compare(txt_word.Text, string.Empty) != 0)
+            {
+                errorProvider1.SetError(txt_word,"");
+                try
+                {
+                    string line;
+                    bool valid = false;
+                    using (StreamReader sr = new StreamReader("dictionary.txt"))
+                    {
+                        // Read and display lines from the file until the end of 
+                        // the file is reached.
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (string.Compare(txt_word.Text, line) == 0)
+                            {
+                                MessageBox.Show("Word " + line + " is Found.","Scrabble Dictionary.");
+                                line = null;
+                                valid = true;
+                            }
+                        }
+                        if (valid == false) MessageBox.Show("Word not found.", "Scrabble Dictionary.");
+                        txt_word.Clear();
+                        txt_word.Focus();
+
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                errorProvider1.SetError(txt_word,"Please enter word.");
+                MessageBox.Show("Please enter word.", "Error");
+                txt_word.Focus();
+            }
+        }
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dialogResult = MessageBox.Show("Do you want to close the application","Scrabble Dictionary",
+                                                        MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+                this.Close();
+            else
+            {
+                txt_word.Clear();
+                txt_word.Focus();
+            }
+                
+        }
+        private void frmScrabble_dictionary_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+        }
+
+        private void frmScrabble_dictionary_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control == true && e.KeyCode == Keys.Enter)
+            {
+                btn_search.PerformClick();
+            }
+
+        }
+    }
+}
